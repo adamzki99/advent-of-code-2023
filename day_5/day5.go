3cb87f6fa29b8a798a78bff5905dbb4dff73d97c
Adam Svensson <therealadamzki@gmail.com>
2023-12-05 16:34:40 +0000

    Solution passing example, result is to low for puzzle_input

---
+++ day_5/day5.go
@@ -0,0 +1,193 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"strconv"
+	"strings"
+
+	"github.com/adamzki99/advent-of-code-2023/packages/file"
+)
+
+type Node struct {
+	data int
+	next *Node
+}
+
+type LinkedList struct {
+	head *Node
+}
+
+func (ll *LinkedList) Append(data int) {
+	newNode := &Node{data: data, next: nil}
+
+	if ll.head == nil {
+		ll.head = newNode
+		return
+	}
+
+	current := ll.head
+	for current.next != nil {
+		current = current.next
+	}
+	current.next = newNode
+}
+
+func (ll *LinkedList) Display() {
+	current := ll.head
+	for current != nil {
+		fmt.Printf("%d -> ", current.data)
+		current = current.next
+	}
+	fmt.Println("nil")
+}
+
+func (ll *LinkedList) Last() *Node {
+
+	currentNode := ll.head
+
+	for {
+
+		if currentNode.next == nil {
+			return currentNode
+		}
+
+		currentNode = currentNode.next
+	}
+
+}
+
+type Mapping struct {
+	lowerBound int
+	upperBound int
+	change     int
+}
+
+func StringOfNumbersToSliceOfNumbers(stringOfNumbers, seperator string) []int {
+
+	seperateValues := strings.Split(stringOfNumbers, seperator)
+
+	returnSlice := []int{}
+
+	for _, v := range seperateValues {
+
+		convertedIntegers, err := strconv.Atoi(v)
+
+		if err != nil {
+			return []int{}
+		}
+
+		returnSlice = append(returnSlice, convertedIntegers)
+	}
+
+	return returnSlice
+}
+
+func CreateMappings(mapInput string) []Mapping {
+
+	mappings := []Mapping{}
+
+	mapInputLines := strings.Split(mapInput, "\n")
+	mapInputLines = mapInputLines[1:]
+
+	for _, line := range mapInputLines {
+		extractedValuesFromLine := StringOfNumbersToSliceOfNumbers(line, " ")
+
+		if len(extractedValuesFromLine) == 0 {
+			return []Mapping{}
+		}
+
+		newMapping := Mapping{
+			lowerBound: extractedValuesFromLine[1],
+			upperBound: extractedValuesFromLine[1] + extractedValuesFromLine[2] - 1,
+			change:     extractedValuesFromLine[0] - extractedValuesFromLine[1],
+		}
+
+		mappings = append(mappings, newMapping)
+	}
+
+	return mappings
+}
+
+func CreateListOfSeeds(fileContent *string) []int {
+
+	fileLines := strings.Split(*fileContent, "\n")
+
+	return StringOfNumbersToSliceOfNumbers(strings.Split(fileLines[0], ": ")[1], " ")
+
+}
+
+func SolvePuzzle(fileName string) int {
+
+	// Instead of generating the complete maps, we could instead just calculate the next value in the linked list
+	// The next value can maybe be identified before it is calculated.
+	// Like is it between the upper an lower bounds of a calculated value, or will it inherent the same value as it has?
+
+	fileContent, err := file.ReadFileContents(fileName)
+
+	if err != nil {
+		fmt.Println(err)
+		return -1
+	}
+
+	listOfSeeds := []LinkedList{}
+
+	for _, seed := range CreateListOfSeeds(&fileContent) {
+
+		newLinkedList := LinkedList{}
+		newLinkedList.Append(seed)
+
+		listOfSeeds = append(listOfSeeds, newLinkedList)
+	}
+
+	mapStrings := strings.Split(fileContent, "\n\n")
+	mapStrings = mapStrings[1:]
+
+	for _, mapString := range mapStrings {
+
+		mappings := CreateMappings(mapString)
+
+	seedLoop:
+		for _, seed := range listOfSeeds {
+
+			for _, mapping := range mappings {
+
+				if mapping.lowerBound <= seed.Last().data && seed.Last().data <= mapping.upperBound {
+					seed.Append(seed.Last().data + mapping.change)
+					continue seedLoop
+				}
+
+			}
+
+			seed.Append(seed.Last().data)
+
+		}
+	}
+
+	//for _, seed := range listOfSeeds {
+
+	//seed.Display()
+
+	//}
+
+	// Now we can get the lowest location
+
+	lowestLocation := math.MaxUint32
+
+	for _, seed := range listOfSeeds {
+
+		//fmt.Println(seed.Last().data)
+
+		if seed.Last().data < lowestLocation {
+			lowestLocation = seed.Last().data
+		}
+
+	}
+
+	return lowestLocation
+}
+
+func main() {
+
+	fmt.Println(SolvePuzzle("puzzle_input.txt"))
+}
---
+++ day_5/day5_test.go
@@ -0,0 +1,258 @@
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+func areIntSlicesEqual(slice1, slice2 []int) bool {
+	if len(slice1) != len(slice2) {
+		return false
+	}
+
+	for i, v := range slice1 {
+
+		if v != slice2[i] {
+			return false
+		}
+
+	}
+	return true
+}
+
+func TestAppend(t *testing.T) {
+
+	ll := LinkedList{}
+
+	ll.Append(1)
+	result := ll.head.data
+	expected := 1
+
+	if result != expected {
+		t.Errorf("CreateMap function test failed. Expected %d. Got %d", expected, result)
+	}
+
+	ll.Append(2)
+	result = ll.head.next.data
+	expected = 2
+
+	if result != expected {
+		t.Errorf("CreateMap function test failed. Expected %d. Got %d", expected, result)
+	}
+
+}
+
+func TestLast(t *testing.T) {
+
+	ll := LinkedList{}
+
+	ll.Append(1)
+	ll.Append(2)
+	result := ll.Last().data
+	expected := 2
+
+	if result != expected {
+		t.Errorf("CreateMap function test failed. Expected %d. Got %d", expected, result)
+	}
+}
+
+func TestStringOfNumbersToSliceOfNumbers(t *testing.T) {
+
+	result := StringOfNumbersToSliceOfNumbers("88 18 7", " ")
+	expected := []int{88, 18, 7}
+
+	if !areIntSlicesEqual(result, expected) {
+		t.Error("StringOfNumbersToSliceOfNumbers function test 1 failed.")
+	}
+
+	result = StringOfNumbersToSliceOfNumbers("0 69 1", " ")
+	expected = []int{0, 69, 1}
+
+	if !areIntSlicesEqual(result, expected) {
+		t.Error("StringOfNumbersToSliceOfNumbers function test 2 failed.")
+	}
+
+	result = StringOfNumbersToSliceOfNumbers("52 50 48", " ")
+	expected = []int{52, 50, 48}
+
+	if !areIntSlicesEqual(result, expected) {
+		t.Error("StringOfNumbersToSliceOfNumbers function test 3 failed.")
+	}
+
+}
+
+func TestCreateMapping(t *testing.T) {
+
+	input := `seed-to-soil map:
+50 98 2
+52 50 48`
+
+	result := CreateMappings(input)
+	expected := []Mapping{
+		{
+			lowerBound: 98,
+			upperBound: 98 + 2 - 1,
+			change:     50 - 98,
+		},
+		{
+			lowerBound: 50,
+			upperBound: 50 + 48 - 1,
+			change:     52 - 50,
+		},
+	}
+
+	indexToExamine := 0
+	if result[indexToExamine].upperBound != expected[indexToExamine].upperBound || result[indexToExamine].lowerBound != expected[indexToExamine].lowerBound || result[indexToExamine].change != expected[indexToExamine].change {
+		t.Errorf("CreateMappings function test failed.")
+	}
+	indexToExamine = 1
+	if result[indexToExamine].upperBound != expected[indexToExamine].upperBound || result[indexToExamine].lowerBound != expected[indexToExamine].lowerBound || result[indexToExamine].change != expected[indexToExamine].change {
+		t.Errorf("CreateMappings function test failed.")
+	}
+
+	input = `soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15`
+
+	result = CreateMappings(input)
+	expected = []Mapping{
+		{
+			lowerBound: 15,
+			upperBound: 15 + 37 - 1,
+			change:     0 - 15,
+		},
+		{
+			lowerBound: 52,
+			upperBound: 52 + 2 - 1,
+			change:     37 - 52,
+		},
+		{
+			lowerBound: 0,
+			upperBound: 0 + 15 - 1,
+			change:     39 - 0,
+		},
+	}
+
+	indexToExamine = 0
+	if result[indexToExamine].upperBound != expected[indexToExamine].upperBound || result[indexToExamine].lowerBound != expected[indexToExamine].lowerBound || result[indexToExamine].change != expected[indexToExamine].change {
+		t.Errorf("CreateMappings function test failed.")
+	}
+	indexToExamine = 1
+	if result[indexToExamine].upperBound != expected[indexToExamine].upperBound || result[indexToExamine].lowerBound != expected[indexToExamine].lowerBound || result[indexToExamine].change != expected[indexToExamine].change {
+		t.Errorf("CreateMappings function test failed.")
+	}
+	indexToExamine = 2
+	if result[indexToExamine].upperBound != expected[indexToExamine].upperBound || result[indexToExamine].lowerBound != expected[indexToExamine].lowerBound || result[indexToExamine].change != expected[indexToExamine].change {
+		t.Errorf("CreateMappings function test failed.")
+	}
+
+}
+
+func TestCreateListOfSeeds(t *testing.T) {
+
+	content := `seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4`
+
+	result := CreateListOfSeeds(&content)
+	expected := []int{79, 14, 55, 13}
+
+	if !areIntSlicesEqual(result, expected) {
+		t.Error("CreateListOfSeeds function test failed.")
+	}
+
+}
+
+func TestSolvePuzzle(t *testing.T) {
+
+	content := []byte(`seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4`,
+	)
+
+	tmpfile, err := os.CreateTemp("", "example")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Remove temporary test file with the end of the test
+	defer os.Remove(tmpfile.Name())
+
+	// Check write contect to file
+	if _, err := tmpfile.Write(content); err != nil {
+		t.Fatal(err)
+	}
+	if err := tmpfile.Close(); err != nil {
+		t.Fatal(err)
+	}
+
+	result := SolvePuzzle(tmpfile.Name())
+	expected := 35
+
+	// Check if the result matches the expected value
+	if result != expected {
+		t.Errorf("SolvePuzzle function test failed. Expected: %d, Got: %d", expected, result)
+	}
+
+}
