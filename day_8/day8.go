2eb0cf574897e0c273433cc653dbf71b146c85f2
Adam Svensson <therealadamzki@gmail.com>
2023-12-08 13:01:05 +0000

    Part 1 complete

--- day_8/day8.go
+++ day_8/day8.go
@@ -7,81 +7,9 @@ import (
 	"github.com/adamzki99/advent-of-code-2023/packages/file"
 )
 
-type Place struct {
-	current  string
-	left     *Place
-	right    *Place
-	treeRoot *Place
-}
-
-func (p *Place) AppendLeft(place string, tr *Place) {
-
-	if p.current == place {
-		return
-	}
-
-	existingPlace := p.treeRoot.SearchPlaces(place, &[]string{})
-
-	if existingPlace != nil {
-		p.left = existingPlace
-		return
-	}
-
-	p.left = &Place{current: place, treeRoot: p.treeRoot}
-
-}
-
-func (p *Place) AppendRight(place string, tr *Place) {
-
-	if p.current == place {
-		return
-	}
-
-	existingPlace := p.treeRoot.SearchPlaces(place, &[]string{})
-
-	if existingPlace != nil {
-		p.right = existingPlace
-		return
-	}
-
-	p.right = &Place{current: place, treeRoot: p.treeRoot}
-
-}
-
-func sliceContains(s *[]string, e string) bool {
-	for _, v := range *s {
-		if v == e {
-			return true
-		}
-	}
-
-	return false
-}
-
-func (p *Place) SearchPlaces(placeToFind string, visitedPlaces *[]string) *Place {
-
-	var returnPlace *Place
-
-	if p == nil {
-		return returnPlace
-	}
-
-	*visitedPlaces = append(*visitedPlaces, p.current)
-
-	if p.current == placeToFind {
-		return p
-	}
-
-	if p.left != nil && !sliceContains(visitedPlaces, p.left.current) {
-		returnPlace = p.left.SearchPlaces(placeToFind, visitedPlaces)
-	}
-
-	if returnPlace == nil && p.right != nil && !sliceContains(visitedPlaces, p.right.current) {
-		returnPlace = p.right.SearchPlaces(placeToFind, visitedPlaces)
-	}
-
-	return returnPlace
-
+type Paths struct {
+	left  string
+	right string
 }
 
 func CharFactory(s string) func() string {
@@ -133,14 +61,6 @@ func SplitNodeLine(puzzleLine string) (string, string, string) {
 	return return1, return2, return3
 }
 
-func removeElement(slice []string, index int) []string {
-	if index < 0 || index >= len(slice) {
-		return slice
-	}
-
-	return append(slice[:index], slice[index+1:]...)
-}
-
 func SolvePuzzle(fileName string) int {
 
 	fileContent, err := file.ReadFileContents(fileName)
@@ -150,51 +70,24 @@ func SolvePuzzle(fileName string) int {
 		return -1
 	}
 
+	places := make(map[string]Paths)
+
 	puzzleLines := strings.Split(fileContent, "\n")
 
 	nodeLines := puzzleLines[2:]
-	puzzleLineFactory := LineFactory(nodeLines)
 
-	currentLine, _ := puzzleLineFactory()
+	for _, line := range nodeLines {
 
-	p1, p2, p3 := SplitNodeLine(currentLine)
+		p1, p2, p3 := SplitNodeLine(line)
 
-	start := Place{current: p1}
-	start.treeRoot = &start
-	start.AppendLeft(p2, &start)
-	start.AppendRight(p3, &start)
-
-	nodeLines = removeElement(nodeLines, 0)
-	puzzleLineFactory = LineFactory(nodeLines)
-
-	for {
-
-		currentLine, i := puzzleLineFactory()
-
-		p1, p2, p3 = SplitNodeLine(currentLine)
-
-		currentNode := start.SearchPlaces(p1, &[]string{})
-
-		if currentNode == nil {
-			continue
-		}
-
-		currentNode.AppendLeft(p2, &start)
-		currentNode.AppendRight(p3, &start)
-
-		nodeLines = removeElement(nodeLines, i)
-		puzzleLineFactory = LineFactory(nodeLines)
-
-		if len(nodeLines) == 0 {
-			break
-		}
+		places[p1] = Paths{left: p2, right: p3}
 
 	}
 
 	stepFactory := CharFactory(puzzleLines[0])
 	steps := 0
 
-	currentPlace := &start
+	currentPlace := "AAA"
 
 	for {
 
@@ -202,12 +95,13 @@ func SolvePuzzle(fileName string) int {
 		steps++
 
 		if step == "L" {
-			currentPlace = currentPlace.left
+			currentPlace = places[currentPlace].left
 		} else {
-			currentPlace = currentPlace.right
+			currentPlace = places[currentPlace].right
 		}
 
-		if currentPlace.current == "ZZZ" {
+		if currentPlace == "ZZZ" {
+			fmt.Println(places[currentPlace])
 			break
 		}
 
