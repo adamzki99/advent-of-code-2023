f3ecce8ad2f950537a1cee9192f031f8054b58f9
Adam Svensson <therealadamzki@gmail.com>
2023-12-12 14:47:00 +0000

    Test pass

---
+++ day_12/day12.go
@@ -0,0 +1,151 @@
+package main
+
+import (
+	"fmt"
+	"slices"
+	"strconv"
+	"strings"
+
+	"github.com/adamzki99/advent-of-code-2023/packages/file"
+)
+
+type Arrangement struct {
+	local        []string
+	matchesUnder int
+	operational  *Arrangement
+	damaged      *Arrangement
+}
+
+func GetPattern(record []string) []int {
+
+	patterns := []int{}
+
+	brokenCounter := 0
+
+	for _, c := range record {
+
+		if c == "." && brokenCounter != 0 {
+			patterns = append(patterns, brokenCounter)
+			brokenCounter = 0
+		}
+
+		if c == "#" {
+			brokenCounter++
+		}
+
+	}
+
+	if brokenCounter != 0 {
+		patterns = append(patterns, brokenCounter)
+	}
+
+	return patterns
+
+}
+
+func ExtractPattern(line string) []int {
+
+	patternSlice := []int{}
+
+	pattern := strings.Split(line, " ")[1]
+
+	for _, c := range strings.Split(pattern, "") {
+
+		v, e := strconv.Atoi(c)
+
+		if e == nil {
+			patternSlice = append(patternSlice, v)
+		}
+
+	}
+
+	return patternSlice
+}
+
+func PatternMatch(record []string, pattern []int) bool {
+
+	recordPattern := GetPattern(record)
+
+	if len(recordPattern) != len(pattern) {
+		return false
+	}
+
+	for i := range recordPattern {
+
+		if recordPattern[i] != pattern[i] {
+			return false
+		}
+
+	}
+
+	return true
+
+}
+
+func GenerateArrangements(a *Arrangement, pattern []int) {
+
+	if !slices.Contains(a.local, "?") {
+
+		if PatternMatch(a.local, pattern) {
+			a.matchesUnder = 1
+		} else {
+			a.matchesUnder = 0
+		}
+
+		return
+	}
+
+	indexOfChange := slices.Index(a.local, "?")
+
+	a.local[indexOfChange] = "#"
+	localChange := []string{}
+	localChange = append(localChange, a.local...)
+	a.damaged = &Arrangement{local: localChange}
+	GenerateArrangements(a.damaged, pattern)
+
+	a.local[indexOfChange] = "."
+	localChange = []string{}
+	localChange = append(localChange, a.local...)
+	a.operational = &Arrangement{local: localChange}
+	GenerateArrangements(a.operational, pattern)
+
+	// undo change
+	a.local[indexOfChange] = "?"
+	a.matchesUnder = a.damaged.matchesUnder + a.operational.matchesUnder
+}
+
+func SolvePuzzle(fileName string) int {
+
+	fileContent, err := file.ReadFileContents(fileName)
+
+	if err != nil {
+		fmt.Println(err)
+		return -1
+	}
+
+	fileLines := strings.Split(fileContent, "\n")
+
+	puzzleAwnser := 0
+
+	for _, line := range fileLines {
+
+		pattern := ExtractPattern(line)
+
+		lA := strings.Split(line, " ")[0]
+		lineArrangement := strings.Split(lA, "")
+
+		a := &Arrangement{local: lineArrangement}
+
+		GenerateArrangements(a, pattern)
+
+		puzzleAwnser = puzzleAwnser + a.matchesUnder
+
+	}
+
+	return puzzleAwnser
+
+}
+
+func main() {
+	fmt.Println(SolvePuzzle("puzzle_input.txt"))
+}
---
+++ day_12/day12_test.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"os"
+	"testing"
+
+	"github.com/adamzki99/advent-of-code-2023/packages/testhelp"
+)
+
+func TestGetPattern(t *testing.T) {
+
+	result := GetPattern([]string{"#", ".", "#", ".", "#", "#", "#"})
+	expected := []int{1, 1, 3}
+
+	if !testhelp.AreIntSlicesEqual(result, expected) {
+		t.Errorf("GetPattern function test failed.")
+	}
+
+}
+
+func TestExtractPattern(t *testing.T) {
+
+	result := ExtractPattern("#.#.### 1,1,3")
+	expected := []int{1, 1, 3}
+
+	if !testhelp.AreIntSlicesEqual(result, expected) {
+		t.Errorf("ExtractPattern function test failed.")
+	}
+
+	result = ExtractPattern("?#?#?#?#?#?#?#? 1,3,1,6")
+	expected = []int{1, 3, 1, 6}
+
+	if !testhelp.AreIntSlicesEqual(result, expected) {
+		t.Errorf("ExtractPattern function test failed.")
+	}
+
+}
+
+func TestPatternMatch(t *testing.T) {
+
+	result := PatternMatch([]string{"#", ".", "#", ".", "#", "#", "#"}, []int{1, 1, 3})
+	expected := true
+	if result != expected {
+		t.Errorf("PatternMatch function test failed. Expected: %t, Got: %t", expected, result)
+	}
+
+}
+
+func TestGenerateArrangements(t *testing.T) {
+
+	input := &Arrangement{local: []string{"?", ".", "#"}}
+
+	GenerateArrangements(input, []int{1, 1})
+	expected := &Arrangement{
+		local:        []string{"?", ".", "#"},
+		damaged:      &Arrangement{local: []string{"#", ".", "#"}},
+		operational:  &Arrangement{local: []string{".", ".", "#"}},
+		matchesUnder: 1,
+	}
+
+	if !testhelp.AreStringSlicesEqual(input.local, expected.local) ||
+		!testhelp.AreStringSlicesEqual(input.damaged.local, expected.damaged.local) ||
+		!testhelp.AreStringSlicesEqual(input.operational.local, expected.operational.local) ||
+		input.matchesUnder != expected.matchesUnder {
+		t.Errorf("GenerateArrangements function test failed.")
+	}
+
+}
+
+func TestSolvePuzzle(t *testing.T) {
+
+	content := []byte(`???.### 1,1,3
+.??..??...?##. 1,1,3
+?#?#?#?#?#?#?#? 1,3,1,6
+????.#...#... 4,1,1
+????.######..#####. 1,6,5
+?###???????? 3,2,1`)
+
+	tmpfile, err := os.CreateTemp("", "example")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Remove temporary test file with the end of the test
+	defer os.Remove(tmpfile.Name())
+
+	// Check write contect to file
+	if _, err := tmpfile.Write(content); err != nil {
+		t.Fatal(err)
+	}
+	if err := tmpfile.Close(); err != nil {
+		t.Fatal(err)
+	}
+
+	result := SolvePuzzle(tmpfile.Name())
+	expected := 21
+
+	// Check if the result matches the expected value
+	if result != expected {
+		t.Errorf("SolvePuzzle function test failed. Expected: %d, Got: %d", expected, result)
+	}
+
+}
